8e96ed6439a09bf6c76d80ee79b2caa416ef42f7
Varun Palnati <vpalnati@umass.edu>
2025-05-12 15:16:37 -0400

    added spring boot project to application

---
+++ project520/src/main/java/com/_project/project520/Project520Application.java
@@ -0,0 +1,13 @@
+package com._project.project520;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project520Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project520Application.class, args);
+	}
+
+}
