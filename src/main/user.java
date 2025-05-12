eaacdfee536891c7703b3b0bc05fe3daa07f4793
Varun Palnati <vpalnati@umass.edu>
2025-05-12 15:12:15 -0400

    Revert "added spring boot functionality to project"
    This reverts commit d2c225ea6b6a105a146cc35f94b0d381a015c1a0.

--- src/main/user.java
+++ src/main/user.java
@@ -1,14 +1,10 @@
 package main;
-import java.util.List;
-import java.util.ArrayList;
-public class user {
-    //!resultSet.isBeforeFirst()
 
-    private final String username; //hold username
-    private final String password; //hold password
-    private final String role; //rold role
-    private final String email; // hold email
-    private final List<health_info> health_info=new ArrayList<>();
+public class user {
+    private final String username;
+    private final String password;
+    private final String role;
+    private final String email;
     public user(String username, String pass, String email, String role)
     {
         this.username=username;
@@ -31,24 +27,6 @@ public class user {
      public String getrole()
     {
         return this.role;
-    }
-    public boolean addhealthinfo(health_info healthinfo)
-    {
-        if (this.health_info.contains(healthinfo))
-        {
-            System.out.println("health data already added to the list");
-            return false;
-        }
-        else
-        {
-            this.health_info.add(healthinfo);
-            return true;
-
-        }
-    }
-    public  List<health_info> gethealthinfolist()
-    {
-        return this.health_info;
     }
 
 
